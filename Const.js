a1f1ad471f4b018cbee86ace4c02791b2a38217d
hari1538radha <hari.r2020ece@sece.ac.in>
2023-06-02 22:13:09 +0530

    changes commited

--- Const.js
+++ Const.js
@@ -1,4 +1,3 @@
 export const PORT = 5000;
 
-export const mongoUrl = `mongodb+srv://HariR:hari1538@cluster0.u27yv8t.mongodb.net/?retryWrites=true&w=majority`;
-// ${process.env.REACT_APP_DBPASSWORD}
+export const mongoUrl = `mongodb+srv://kamalvimal333:${process.env.REACT_APP_DBPASSWORD}@cluster0.sgw5vwf.mongodb.net/?retryWrites=true&w=majority`;
